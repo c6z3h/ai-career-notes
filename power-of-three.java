111ed9d727cb450062fe84ad474dd679dcae28ad
ZhiHong Chua <chuazhihong@hotmail.com>
2021-05-22 18:03:23 +0800

    Update power-of-three.java

--- power-of-three.java
+++ power-of-three.java
@@ -1,9 +1,14 @@
 class Solution {
     public boolean isPowerOfThree(int n) {
-        while (n>2){
-            n = n/3;
+    /*    float var = n;
+        while (var>2){
+            var = var/3;
         }
-        if (n == 1){return true;}
+        System.out.println(var);
+        if (var == 1){return true;}
         else return false;
-    }
+    }*/
+        // This Quicker Method as Float Method Above Gives Rounding Error.
+        return ( n>0 &&  1162261467%n==0);}
+        
 }
